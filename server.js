f3789c10e072f72fac678fcb52904e363df086d6
Paul Baker <p.baker1@yahoo.com>
2018-08-01 18:06:10 -0500

    ..

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var passport = require('passport');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-// newshit
 
 // For Express
 var app = express();
